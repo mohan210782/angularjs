c899880f1cf722ad86ba0a9384914269ccfa610c
mohan2107 <mohan2107@gmil.com>
2016-10-17 16:36:11 +0530

    changes app js

--- assignment1-lunch-checker/app.js
+++ assignment1-lunch-checker/app.js
@@ -16,7 +16,7 @@
 				{
 					var str = $scope.menu;
 					console.log("Luch Menu: "+str);
-					if($scope.menu != "")
+					if($scope.menu != undefined || null || "")
 					{
 
 						var splits = str.split(',');
@@ -25,8 +25,9 @@
 					
 						console.log("Lunch Menu array: ",splits);
 						console.log("array size:"+splits.length);
+						msg = splits.length == 0 ? "Please enter the data first" :  splits.length <= 3 ?  "enjoy!" : "Too much!";
 
-						msg = (splits.length <= 3)? "Enjoy!" : "Too much!";
+						//msg = (splits.length <= 3 && != 0) ? enjoy!" : "Too much!";
 
 					}
 					else
